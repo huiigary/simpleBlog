00257bd9316c3fc2f81f8d7c2cb592efe2cfc06c
garyhui <gary.hui@sendum.com>
2023-02-06 16:09:34 -0800

    Add listView to only see posts from a user

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     # path('', views.home, name='blog-home'),  # Default path.
     #  using class view....as_view() method converts the class to a view
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
